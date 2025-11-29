cd64eac2daad679341150da61d7912d55d2dd767
Sushil <chaubeyshushil000@gmail.com>
2025-11-29 11:37:12 +0530

    Add varibales

--- variables.py
+++ variables.py
@@ -2,28 +2,28 @@
 name='sushil'
 name=20
 #Type casting
-# last_name=str(name)
-# print(last_name)
-# print(type(last_name))
-# print(name)
+last_name=str(name)
+print(last_name)
+print(type(last_name))
+print(name)
 
 # explore more about variables and data types in python
 x=10,
-# print(type(x))
-# y=str(x)
-# print("y-->",y)
-# print(type(y))
+print(type(x))
+y=str(x)
+print("y-->",y)
+print(type(y))
 # y=10,
 # z=10,
 
 x=y=z=10
-# print(x,y,z)
-# list_of_numbers=[1,2,33,44]
-# print(list_of_numbers)
-# fruits=["apple","banana","mango"]
+print(x,y,z)
+list_of_numbers=[1,2,33,44]
+print(list_of_numbers)
+fruits=["apple","banana","mango"]
 #int , str , float 
 
-# print(fruits)
+print(fruits)
 
 a=10
 b=20
