5074b78c7d47fad2d33f1eaa3b6dff5ba0204b13
Sushil <chaubeyshushil000@gmail.com>
2025-12-06 16:41:39 +0530

    Add string data types

---
+++ string_type.py
@@ -0,0 +1,15 @@
+#String Data type - means sequence of characters - letters, numbers, symbols
+# Examples of string types in Python - "Hello", 'Python123', "2024!",
+
+first_name="Shivam"
+last_name="singh"
+
+print("Value of first_name:",first_name)
+print("Data type of  first_name:",type(first_name))
+
+#Performing some operation on string data types 
+sum_value=first_name+last_name
+print("Value of sum_value : ",sum_value)
+print("Data type of sum_value:",type(sum_value))
+print("Getting the length of sum_value : ",len(sum_value))   # can also we check what data types.
+
