3192d0ad69d68284d4f5d40368a608ecefde79c0
Sushil <chaubeyshushil000@gmail.com>
2025-12-06 16:28:16 +0530

    Add float data type

---
+++ float_types.py
@@ -0,0 +1,22 @@
+#Float Data types - means decimal numbers - positive and negative
+
+# Examples of float types in Python - 0.0,1.5,-5.3,100.25,-250.75
+x=10.5
+y=-100.75
+z=0.0
+w=20.25
+print("Value of x:",x)
+print("Data type of x:",type(x))
+print("Value of y:",y)
+print("Data type of y:",type(y))
+print("Value of z:",z)
+print("Data type of z:",type(z))
+
+# Performing some operation on float types
+sum_result=x+w
+print("Sum of x and w:",sum_result)
+print("Data type of sum_result:",type(sum_result))
+
+difference_result=y-x
+print("Difference of y and x:",difference_result)
+print("Data type of difference_result:",type(difference_result))
