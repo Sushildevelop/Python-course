fd33bcc522872c02561188110d0cf4179460edaf
Sushil <chaubeyshushil000@gmail.com>
2025-12-06 16:17:33 +0530

    Add integer data type

---
+++ integer_types.py
@@ -0,0 +1,26 @@
+#Integer Data types - means whole numbers - positive and negative
+# Examples of integer types in Python - 0,1,-5,100,-250
+a=10
+b=-100
+c=0
+d=20
+print("Value of a:",a)
+print("Data type of a:",type(a))
+print("Value of b:",b)
+print("Data type of b:",type(b))
+print("Value of c:",c)
+print("Data type of c:",type(c))
+
+# Performing some operation on integer types 
+sum_result=a+d
+print("Sum of a and d:",sum_result)
+print("Data type of sum_result:",type(sum_result))
+
+difference_result=b-a
+print("Difference of b and a:",difference_result)
+print("Data type of difference_result:",type(difference_result))
+
+
+
+
+
