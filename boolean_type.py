ca26e196d94d8373fc8f7eb9ca0ba68fd6f70e0f
Sushil <chaubeyshushil000@gmail.com>
2025-12-07 12:11:45 +0530

    Add boolean data types

---
+++ boolean_type.py
@@ -0,0 +1,11 @@
+#Boolean Data types means - True or False.
+# example of Boolean data types - true or false.
+
+a=True
+b=False
+print("Value of a: ",a)
+print("Data type of a: ",type(a))
+add=a+b
+print("Value of add:",add)
+print("Data type of add:",type(add))
+
