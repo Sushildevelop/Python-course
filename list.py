23b697117c96e5335d586155f7bb9826dbf048a4
Sushil <chaubeyshushil000@gmail.com>
2025-12-07 12:21:51 +0530

    add list data types

---
+++ list.py
@@ -0,0 +1,21 @@
+#List  Data type - are used to store multiple items in a single variable
+#example of List - fruit_names=["orange","mango","cherry"]
+
+list_fruits_names=["orange","mango","cherry"]
+list_whole_numbers=[1,2,3,4,5,6]
+list_both=["orange","mango",2,3,4,5]
+
+
+print("Value of list_fruits_names: ",list_fruits_names)
+print("Data type of list fruits_name : ",type(list_fruits_names))
+
+print("Value of list whole number: ",list_whole_numbers)
+print("Data type of list whole number : ",type(list_whole_numbers))
+
+print("Value of list both : ",list_both)
+print("Data types of list both : ",type(list_both))
+
+# if we want to add to list then
+list_add=list_fruits_names+list_whole_numbers
+print("Value of list add : ",list_add)
+print("Data type of list add : ",type(list_add))
