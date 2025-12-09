16e362d618301f13ae9e7af37090f97cd4baea97
Sushil <chaubeyshushil000@gmail.com>
2025-12-09 09:16:09 +0530

    Operators of python

---
+++ Operators/intro.py
@@ -0,0 +1,16 @@
+# Python Operators
+
+# Operators are used to perform operations on variables and values.
+
+a=10 
+b=5
+print(a + b)
+print(a-b)
+print(a**b)
+print(a*b)
+print(a/b)
+print(a+2*b)
+
+
+
+
