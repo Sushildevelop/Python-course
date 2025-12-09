f0ad772ee0be7e92b399faa10e2a2549a164bcb0
Sushil <chaubeyshushil000@gmail.com>
2025-12-09 09:21:55 +0530

    explain arithmetic operator of python

---
+++ Operators/arithmetic_operators.py
@@ -0,0 +1,11 @@
+
+# Arithmetic operators are used with numeric values to perform common mathematical operations
+
+#       Operator	        Name	                    Example
+#       +	                Addition	                x + y	
+#       -	                Subtraction	                x - y	
+#       *	                Multiplication	            x * y	
+#       /	                Division	                x / y	
+#       %	                Modulus	                    x % y	
+#       **	                Exponentiation	            x ** y	
+#       //	                Floor division	            x // y
