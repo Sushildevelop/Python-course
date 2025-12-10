0fa31a54cc4f18c294d51fa7e6377b2a01ee8d54
Sushil <chaubeyshushil000@gmail.com>
2025-12-10 23:16:20 +0530

    add example in arithmetic operations

--- Operators/arithmetic_operators.py
+++ Operators/arithmetic_operators.py
@@ -9,3 +9,17 @@
 #       %	                Modulus	                    x % y	
 #       **	                Exponentiation	            x ** y	
 #       //	                Floor division	            x // y
+
+
+
+# Perform operation using Arithmetic Operations
+x = 15
+y = 4
+
+print("addition operations: ",x + y)
+print("Subtraction operations : ",x - y)
+print("multiplication operations: ",x * y)
+print("division operations: ",x / y)
+print("modulus operations: ",x % y)
+print("Exponentiation operations : ",x ** y)
+print("Floor division: ", x // y)
