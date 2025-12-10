fc5a3b60cfa41e7a79d43c19f07bae9e7685546a
Sushil <chaubeyshushil000@gmail.com>
2025-12-10 23:48:36 +0530

    add comparison operators

---
+++ Operators/comparison_operators.py
@@ -0,0 +1,10 @@
+# Comparison Operators
+# Comparison operators are used to compare two values
+
+#       Operator	    Name	                        Example	
+#         ==	        Equal	                        x == y	
+#         !=	        Not equal	                    x != y	
+#         >	            Greater than	                x > y	
+#         <	            Less than	                    x < y	
+#         >=	        Greater than or equal to	    x >= y	
+#         <=	        Less than or equal to	        x <= y
