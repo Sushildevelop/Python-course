d8a7dcc915686aa25c0a52df0047f7f8f6973ddd
Sushil <chaubeyshushil000@gmail.com>
2026-01-01 21:54:51 +0530

    add Membership operators

---
+++ Operators/membership_operators.py
@@ -0,0 +1,16 @@
+# Membership Operators
+
+# Membership operators are used to test if a sequence is presented in an object
+# It returns True or False
+
+#       Operator	Name	        Example
+#         in	    Membership	    x in y
+#         not in	Not Membership	x not in y
+
+x = ["apple", "banana", "cherry"]
+
+print("banana in x: ", "banana" in x)
+print("pineapple not in x: ", "pineapple" not in x)
+
+y = "Hello world"
+print("H in y: ", "P" in y)
